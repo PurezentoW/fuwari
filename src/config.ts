80ebc4597bb5818b244070cf058ebc90e3eb4230
Purezentow <silenceteaaaaa@gmail.com>
2024-10-25 18:34:58 +0800

    补充

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'Purezento',
-  subtitle: 'Purezento の 自留地',
+  subtitle: 'Purezento的自留地',
   lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
