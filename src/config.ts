387fa6f5f461e8d77b817770f421a4c3f9111fdc
Purezentow <silenceteaaaaa@gmail.com>
2025-02-27 10:01:16 +0800

    feat(config): 更新个人资料配置- 将头像路径改为从 /assets 开始，确保正确加载

--- src/config.ts
+++ src/config.ts
@@ -52,11 +52,11 @@ export const profileConfig: ProfileConfig = {
   bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   links: [
     {
-      name: 'Wechat',
-      icon: 'ic:baseline-wechat',       // Visit https://icones.js.org/ for icon codes
+      name: 'Twitter',
+      icon: 'fa6-brands:twitter',        // Visit https://icones.js.org/ for icon codes
                                         // You will need to install the corresponding icon set if it's not already included
                                         // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://wx.qq.com',
+      url: 'https://twitter.com',
     },
     {
       name: 'Steam',
